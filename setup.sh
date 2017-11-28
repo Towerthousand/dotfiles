a6be10719542e7a57bd35239e3883fd1be181a24
Towerthousand <dani.torramilans@gmail.com>
2017-11-28 13:36:04 +0100

    Add sudoing

--- setup.sh
+++ setup.sh
@@ -22,11 +22,11 @@ rm -rf fonts
 
 echo 'Installing battery plugin...'
 curl -O https://raw.githubusercontent.com/holman/spark/master/spark
-mv spark /usr/local/bin
-chmod u+x /usr/local/bin/spark
+sudo mv spark /usr/local/bin
+sudo chmod u+x /usr/local/bin/spark
 
 curl -O https://raw.githubusercontent.com/goles/battery/master/battery
-mv battery /usr/local/bin
-chmod u+x /usr/local/bin/battery
+sudo mv battery /usr/local/bin
+sudo chmod u+x /usr/local/bin/battery
 
 echo 'Remember to install YCM stuff, and switch the terminal font & colors.'
